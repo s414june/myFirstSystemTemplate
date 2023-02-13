b8ff00f173d7fab3f7fc5c371db8679beb74665a
personal <s414june@gmail.com>
2023-02-13 11:25:07 +0800

    調整手機版介面;程式碼除錯

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,30 @@
 // @ts-nocheck
 $(function () {
     //手機高度
-    $("html").css("--vh", $(window).width());
+    $("html").css("--vh", $(window).height());
     const maxMenuWidth = $("html").css("--menu-width");
+    const isLessThanMedium = $(window).width() < 768;
+    if (isLessThanMedium) toggleMenuWidth();
     $(".menu-btn").click(() => {
+        toggleMenuWidth();
+    })
+    $('#example').DataTable({
+        searching: false
+    });
+    $(window).resize(() => {
+        //手機高度
+        $("html").css("--vh", $(window).height());
+        const isLessThanMedium = $(window).width() < 768;
+        if (isLessThanMedium) $("#menu").hide();
+    })
+
+    function toggleMenuWidth() {
         $("#menu").toggle();
         let nowWidth = $("html").css("--menu-width");
         let nextWitdh = 0;
         nextWitdh = nowWidth === maxMenuWidth ? 0 : maxMenuWidth;
         $("html").css("--menu-width", nextWitdh);
-    })
-    $('#example').DataTable({
-        searching: false
-    });
-    $(window).resize(()=>{
-        //手機高度
-        $("html").css("--vh", $(window).width());
-    })
+    }
 })
 // $(document).ready(function () {
 //     // searching: false
