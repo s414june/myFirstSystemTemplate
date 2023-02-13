624bfbad31be975ef2cb6e150ee305c79b5d23cb
personal <s414june@gmail.com>
2023-02-13 11:36:12 +0800

    調整手機版介面;程式碼除錯

--- scripts/script.js
+++ scripts/script.js
@@ -1,22 +1,28 @@
 // @ts-nocheck
 $(function () {
     //手機高度
-    $("html").css("--vh", $(window).width());
+    $("html").css("--vh", $(window).height());
     const maxMenuWidth = $("html").css("--menu-width");
+    if($(window).width()<768){
+        toggleMenu();
+    }
     $(".menu-btn").click(() => {
-        $("#menu").toggle();
-        let nowWidth = $("html").css("--menu-width");
-        let nextWitdh = 0;
-        nextWitdh = nowWidth === maxMenuWidth ? 0 : maxMenuWidth;
-        $("html").css("--menu-width", nextWitdh);
+        toggleMenu();
     })
     $('#example').DataTable({
         searching: false
     });
     $(window).resize(()=>{
         //手機高度
-        $("html").css("--vh", $(window).width());
+        $("html").css("--vh", $(window).height());
     })
+    function toggleMenu(){
+        $("#menu").toggle();
+        let nowWidth = $("html").css("--menu-width");
+        let nextWitdh = 0;
+        nextWitdh = nowWidth === maxMenuWidth ? 0 : maxMenuWidth;
+        $("html").css("--menu-width", nextWitdh);
+    }
 })
 // $(document).ready(function () {
 //     // searching: false
